07d8c2d26fad1caca228b74268ff5e091a06db8e
Priyadharshan <rkdharshan007@gmail.com>
2026-02-18 15:40:43 +0530

    v1.0 Base Classification Model (Logistic Regression) without pipeline

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,12 +2,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, cross_val_score
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 def load_model(path):
     df = pd.read_csv(path)
     x = df.drop("target", axis=1)
     y = df["target"]
-    model = LogisticRegression()
+    model = RandomForestClassifier()
     crossval = cross_val_score(model, x, y, cv=5)
     xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=32)
     return xtrain, xtest, ytrain, ytest, crossval.mean()
