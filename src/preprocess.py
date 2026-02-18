929dcfd5c33d23676576bb59a5f3a6c712b15ad2
Priyadharshan <rkdharshan007@gmail.com>
2026-02-18 15:50:27 +0530

    Base Logistic Regression classification model

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,7 +8,7 @@ def load_model(path):
     df = pd.read_csv(path)
     x = df.drop("target", axis=1)
     y = df["target"]
-    model = RandomForestClassifier()
+    model = LogisticRegression(max_iter = 1500)
     crossval = cross_val_score(model, x, y, cv=5)
     xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=32)
     return xtrain, xtest, ytrain, ytest, crossval.mean()
--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 import joblib
 def train(xscaled, ytrain):
-    model = RandomForestClassifier()
+    model = LogisticRegression(max_iter = 1500)
     model.fit(xscaled, ytrain)
     return model
 def save_model(model, path):
